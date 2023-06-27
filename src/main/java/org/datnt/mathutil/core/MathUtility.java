f9783778d090056de57de727d731c7e8ace5f0d0
datntdev02 <datntdev02@gmail.com>
2023-06-27 14:35:51 +0700

    add screenshots; update source using recursion to see green4ever

--- src/main/java/org/datnt/mathutil/core/MathUtility.java
+++ src/main/java/org/datnt/mathutil/core/MathUtility.java
@@ -21,19 +21,37 @@ public class MathUtility {
     // vì giai thừa tăng rất nhanh, 21! LONG k chứa nổi (LONG 18 số 0)
     // 20! vừa đủ cho LONG
     // 21! 22! 23! > 20! -> CHỬI, ném ra Exeption
+//    public static long getFactorial(int n) {
+//        
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n: n must be between 0..20");
+//        
+//        if (n == 0 || n == 1)
+//        return 1;
+//        
+//        long product = 1; // Biến lưu dồn các tích
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//    }
     public static long getFactorial(int n) {
-        
-        if (n < 0 || n > 20)
+
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n: n must be between 0..20");
+        }
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
         
-        if (n == 0 || n == 1)
-        return 1;
+        return n * getFactorial(n-1);
         
-        long product = 1; // Biến lưu dồn các tích
-        for (int i = 2; i <= n; i++)
-            product *= i;
-        
-        return product;
     }
 }
 
+//  5! = 1.2.3.4.5 = 1.2.3.4   . 5 = 5 x 4!
+//  4! = 4 x 3!
+//  3! = 3 x 2!
+//  2! = 2 x 1!
+//  1! = 1; dừng quá trình
